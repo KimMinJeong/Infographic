07cd2354dfd784483807de283af50a9734107b21
KIM <zooey@spoqa.com>
2014-03-27 22:06:30 +0900

    test

---
+++ infographic.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, g, session, flash, redirect, \
+    url_for, jsonify, json
+from flask_openid import OpenID
+from functools import wraps
+from os import environ
+from os.path import dirname, join
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql import functions
+from sqlalchemy.types import DateTime, Boolean
+import hashlib
+import os
+import urllib
+import json, requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+@app.route('/editor/<int:id>')
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=int(environ.get('PORT',5000)))
