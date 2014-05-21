b68a0640ed45075e9ccee24950220ee983b4dd90
KIM <zooey@spoqa.com>
2014-05-21 16:43:57 +0900

    Possible new button, modal dialog

--- infographic.py
+++ infographic.py
@@ -93,14 +93,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/test', methods=['GET'])
+def test():
+    return render_template('test.html')
+
+
 @app.route('/editor', methods=['GET'])
 def editor():
     return render_template('tool.html')
 
-
-@app.route('/test', methods=['GET'])
-def test():
-    return render_template('test.html')
 
 @app.route('/test2',methods=['GET'])
 def test2():
@@ -140,5 +141,10 @@ def logout():
     session.pop()
     return redirect('/index')
     
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=int(environ.get('PORT',5000)))
