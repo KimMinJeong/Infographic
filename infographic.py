3205c467b7e67ef8f39b40d7a57849c308992d43
KIM <zooey@spoqa.com>
2014-06-05 14:33:17 +0900

    finishi

--- infographic.py
+++ infographic.py
@@ -215,4 +215,4 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=int(environ.get('PORT',33507)))
+    app.run(debug=True,host='0.0.0.0', port=int(environ.get('PORT',5000)))
